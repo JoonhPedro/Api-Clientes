1a6d412672044f34cc83375c6a2de5c6671b7dee
João Pedro <joao.veo77@gmail.com>
2024-05-15 23:13:34 -0300

    node-deploy variavel env

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import cors from '@fastify/cors'
+import dotenv from 'dotenv'
 import Fastify from 'fastify'
 import { routes } from './routes'
+
+dotenv.config()
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL)
 
 const app = Fastify({ logger: true })
 
